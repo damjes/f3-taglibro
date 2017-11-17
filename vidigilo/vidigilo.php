daa1a1cf22bdeb8f27a6f19da56b085854396e60
Damjes <c9.ksrtbs@damj.es>
2017-11-17 14:42:47 +0000

    Vidigilo funkcias!

--- vidigilo/vidigilo.php
+++ vidigilo/vidigilo.php
@@ -4,6 +4,7 @@ class Vidigilo
 {
 	public static function malfona($titolo, $tipo)
 	{
+		$f3 = Base::instance();
 		$f3->set('titolo', $titolo . ' – ' . $f3->get('aspekto.retejnomo'));
 		$f3->set('tipo', $tipo);
 		echo Template::instance()->render('vidigilo/malfona.html');
